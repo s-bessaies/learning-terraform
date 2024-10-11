489a5c0ab4efc7e7636cc3230800f620764c26a9
Sami Bessaies <sami.bessaies@gmail.com>
2024-10-11 18:26:47 +0100

    fix lambda response & zip it

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,6 +1,17 @@
+import json
+
 def lambda_handler(event, context):
    print(event)
    message = 'Hello from there from Lambda!' #This is thessage
-   return {
-       'message' : message
-   }
+
+   response = {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": message,
+        "isBase64Encoded": false
+    }
+
+   return json.dumps(response)
+   
