f8580720f4420ec4990160ee3b1a59f1d8bfcf55
Sami Bessaies <sami.bessaies@gmail.com>
2024-10-11 09:17:52 +0100

    create lambda

--- main.tf
+++ main.tf
@@ -1,24 +1,81 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
+resource "aws_iam_role" "lambda_role" {
+  name               = "terraform_aws_lambda_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+# IAM policy for logging from a lambda
 
-  tags = {
-    Name = "HelloWorld"
-  }
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# Policy Attachment on the role.
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+# Generates an archive from content, a file, or a directory of files.
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/python/"
+  output_path = "${path.module}/python/hello-python.zip"
+}
+
+# Create a lambda function
+# In terraform ${path.module} is the current directory.
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename      = "${path.module}/python/hello-python.zip"
+  function_name = "Jhooq-Lambda-Function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "hello-python.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
+output "teraform_aws_role_output" {
+  value = aws_iam_role.lambda_role.name
+}
+
+output "teraform_aws_role_arn_output" {
+  value = aws_iam_role.lambda_role.arn
+}
+
+output "teraform_logging_arn_output" {
+  value = aws_iam_policy.iam_policy_for_lambda.arn
 }
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,22 +1,5 @@
-import json
-
 def lambda_handler(event, context):
-    bedrock = boto3.client(
-        service_name='bedrock-runtime',
-        region_name='us-east-1'
-    )
-    
-    input = {
-        "modelId": "cohere.command-text-v14",
-        "contentType": "application/json",
-        "accept": "*/*",
-        "body": "{\"prompt\":\"What is AWS ?\",\"max_tokens\":400,\"temperature\":0.75,\"p\":0.01,\"k\":0, \"stop_sequences\":[],\"return_likelihoods\":\"NONE\"}"
-    }
-    
-    response = bedrock.invoke_model(body=input["body"],
-                                    modelId=input["modelId"],
-                                    accept=input["accept"],
-                                    contentType=input["contentType"])
-                                    
-    response_body = json.loads(response['body'].read())
-    print(response_body)
+   message = 'Hello {} !'.format(event['key1'])
+   return {
+       'message' : message
+   }
