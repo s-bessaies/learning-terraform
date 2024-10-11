a7fa20352ccecb25d8610a3da47b308be6aebcae
Sami Bessaies <sami.bessaies@gmail.com>
2024-10-11 09:20:17 +0100

    fix terraform issues

--- main.tf
+++ main.tf
@@ -53,17 +53,17 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
-  source_dir  = "${path.module}/python/"
-  output_path = "${path.module}/python/hello-python.zip"
+  source_dir  = "${path.module}/src/"
+  output_path = "${path.module}/src/lambda_function.zip"
 }
 
 # Create a lambda function
 # In terraform ${path.module} is the current directory.
 resource "aws_lambda_function" "terraform_lambda_func" {
-  filename      = "${path.module}/python/hello-python.zip"
+  filename      = "${path.module}/python/lambda_function.zip"
   function_name = "Jhooq-Lambda-Function"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "hello-python.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
