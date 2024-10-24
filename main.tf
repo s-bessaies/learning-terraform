a607f745f2da5d73edba2debeea40609e73e03ac
Sami Bessaies <sami.bessaies@gmail.com>
2024-10-24 11:04:33 +0100

    update timeout

--- main.tf
+++ main.tf
@@ -72,6 +72,7 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
+  timeout       = 900
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
   depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
