a1d7583630216d6c32b7b4ec63a4e71559360122
Sami Bessaies <sami.bessaies@gmail.com>
2024-10-24 10:13:23 +0100

    add bedrock

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,4 +1,5 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
    print(event)
@@ -9,14 +10,43 @@ def lambda_handler(event, context):
         body = json.loads(event['body'])
         
         print('POST Body:', body)
-        print('param1:',body.get("param1"))
+        print('param1:',body.get("param1")) 
+        param1 = body.get("param1")       
+        config = {
+            "prompt":param1,
+            "max_tokens":400,
+            "temperature":0.75,
+            "p":0.01,
+            "k":0, 
+            "stop_sequences":[],
+            "return_likelihoods":"NONE"
+        }
+
+        input = {
+            "modelId": "cohere.command-text-v14",
+            "contentType": "application/json",
+            "accept": "*/*",
+            "body": json.dumps(config)
+        }
+
+        bedrock = boto3.client(
+                service_name='bedrock-runtime',
+                region_name='us-east-1'
+        )
+            
+        response = bedrock.invoke_model(body=input["body"],
+                                            modelId=input["modelId"],
+                                            accept=input["accept"],
+                                            contentType=input["contentType"])
+                                            
+        response_body = json.loads(response['body'].read())
 
    response = {
         "statusCode": 200,
         "headers": {
             "my_header": "my_value"
         },
-        "body": message,
+        "body": response_body,
         "isBase64Encoded": False
     }
 
