f9b6085166deb773186fca9aecbea66994bd2d15
Sami Bessaies <sami.bessaies@gmail.com>
2024-10-24 09:52:30 +0100

    extract param1

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,15 @@
-
+import json
 
 def lambda_handler(event, context):
    print(event)
    message = 'Hello from there from Lambda!' #This is thessage
+
+   if 'body' in event:
+        # The body is usually a JSON string, so parse it
+        body = json.loads(event['body'])
+        
+        print('POST Body:', body)
+        print('param1:',body.get("param1"))
 
    response = {
         "statusCode": 200,
---
+++ src/run.sh
@@ -0,0 +1,2 @@
+rm lambda_function.zip
+zip lambda_function.zip lambda_function.py  
